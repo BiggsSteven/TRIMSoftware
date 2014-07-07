30a63ba6a059a2f732335dc92d193d545b90a622
BiggsSteven <Biggs.Steven.M@gmail.com>
2014-07-07 17:57:12 -0400

    Added new dataSet

---
+++ Suntech/UpdateTechFields.sql
@@ -0,0 +1,13 @@
+﻿ALTER TABLE dbo.Technicians
+ADD FirstName nvarchar(50),
+MiddleInitial char(10),
+LastName nvarchar(50),
+HomeAddress nvarchar(50),
+PayPercentage numeric(18,2),
+SSN nvarchar(50),
+FedIDNum nvarchar(50),
+PhoneNum nvarchar(50),
+EmailAdd nvarchar(50)
+
+UPDATE dbo.Technicians
+set PayPercentage=.70
