00691a14727ce6622fea9b9b90424e8512071847
BiggsSteven <Biggs.Steven.M@gmail.com>
2015-02-12 17:18:10 -0500

    IDK

---
+++ TRIMsoftware/2-12-2015Heath.sql
@@ -0,0 +1,12 @@
+﻿Alter Table Technicians
+ADD Active Bit
+
+Update Technicians
+SET Active = True
+
+CREATE TABLE ImportHist
+(
+FileImport nvarchar(50) NOT NULL,
+Date Datetime,
+PRIMARY KEY (FileImport)
+);
